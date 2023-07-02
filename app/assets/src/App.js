16c35c2029cf481b2aa161c52c5f33da07a40e77
thiagobenedicto <benedict.thiago@gmail.com>
2023-07-02 01:19:30 -0300

    ticket update function creation

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -5,15 +5,6 @@ client.on('app.registered', (e) => {
   client.invoke('resize', { width: '100%', height: '230px' });
 });
 
-const options = {
-  url: 'https://api.example.com',
-  type: 'GET',
-};
-
-client.request(options).then((response) => {
-  console.log(response);
-});
-
 // Create screen context
 import Main from './Main.js';
 Main();
--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,9 +1,29 @@
-function updateSubject() {
-  // Obtém o valor digitado no campo digite o novo assunto
-  let subject = document.getElementById('text-id').value;
+let client = ZAFClient.init();
 
-  // Faz algo com o resultado
-  console.log('Valor digitado: ' + subject);
+// Obtém o valor digitado no campo digite o novo assunto
+async function updateSubject() {
+  // Pega o valor digitado no input no iframe
+  const subject = document.getElementById('text-id').value;
+
+  // Retorna a response com o ID do ticket
+  const ticketId = (await client.get('ticket.id'))['ticket.id'];
+
+  // Pega data e hora atual e substitui a virgula por espaço e hifen
+  const datetime = new Date().toLocaleString().replace(',', ' -');
+
+  // options
+  const options = {
+    url: `/api/v2/tickets/${ticketId}`,
+    type: 'PUT',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      ticket: {
+        subject: `${subject} ${datetime}`,
+      },
+    }),
+  };
+
+  const responseRequest = await client.request(options);
 }
 
 const Core = {
